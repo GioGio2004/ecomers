1cf1791835f4336beb3ad2db6e5df9c463b5f1d3
GioGio2004 <giokhvichia69@gmail.com>
2024-12-03 23:40:31 +0400

    middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,11 @@
-import express from 'express'
+import express, {json, urlencoded} from 'express'
 import productsRouter from './routes/products/index'
 
 
-const app = express();
-
 const port = 3000
+const app = express();
+app.use(urlencoded( {extended: false}))
+app.use(json())
 
 
 app.get('/', (req, res) =>{
