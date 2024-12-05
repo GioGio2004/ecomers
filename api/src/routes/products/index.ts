728ccfd18e82bd9c44ca9046c94ac1ce3e594a68
GioGio2004 <giokhvichia69@gmail.com>
2024-12-05 20:10:28 +0400

    data validation

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,13 +1,18 @@
 import { Router } from "express"
 import { listProducts, getProductById, createProduct, updateProduct, deleteProduct } from "./productController"
+import { validateData } from "../../middlewares/validationMiddleware"
 
+import { createProductSchema, updateProductSchema } from "../../db/productSchema";
+
+
+// type ProductType = z.infer<typeof createProductSchema>
 
 const router = Router()
 
 router.get('/', listProducts)
 router.get('/:id', getProductById)
-router.post('/', createProduct)  
-router.put('/:id', updateProduct)  
+router.post('/',validateData(createProductSchema), createProduct)  
+router.put('/:id',validateData(updateProductSchema) ,updateProduct)  
 router.delete('/:id', deleteProduct)  
 
 
