c7e02f22955b0aff5706c80439cb09c57520b9eb
GioGio2004 <giokhvichia69@gmail.com>
2024-12-05 18:37:26 +0400

    product CRUD

--- api/drizzle/0002_clear_cardiac.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "products" ADD COLUMN "age" double precision NOT NULL;
--- api/drizzle/0003_nostalgic_shadowcat.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "products" ALTER COLUMN "age" SET DATA TYPE integer;--> statement-breakpoint
-ALTER TABLE "products" ALTER COLUMN "age" DROP NOT NULL;
